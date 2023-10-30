cedebdae82da7be5840a915865c847baf66c39bc
Omelnikova <ommelnikova@gmail.com>
2023-10-30 12:58:53 +0100

    change name

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'User Management API',
+      title: 'IT Switcher API',
       version: '1.0.0',
       description: 'A simple Express API for user management',
     },
